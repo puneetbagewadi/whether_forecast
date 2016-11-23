424260e741cb34bb4c81e7cca65279854fab1b05
Puneet <Puneet>
2016-11-23 12:27:03 +0530

    displaying api data day wise

--- js/app.js
+++ js/app.js
@@ -29,10 +29,45 @@
 			});
 			
 		}
-	});
 
-	$(window).load(function(){
+		// Call Weather API and get the response in JSON
 
+		// Callback functions
+
+		// Success callback
+		var updatePage = function( response ) {
+			console.log("API response");
+		  	console.log(response);
+		  	var prevDate = '';
+		  	jQuery.each( response.list, function(index, value) {
+		  		
+   				if(new Date(value.dt_txt).getDate() === prevDate){
+   					;
+   				}else{
+   					prevDate = new Date(value.dt_txt).getDate();
+   					value.city_name = response.city.name;
+   					$( "#weatherTemplate" ).tmpl( value ).appendTo( "#forecast-tab" );
+   				}
+			});
+
+		  	
+
+		};
+
+		// Error callback
+		var displayError = function( req, status, err ) {
+		  console.log( 'something went wrong', status, err );
+		};
+
+		var weatherOptions = {
+		  url: 'http://api.openweathermap.org/data/2.5/forecast?id=1277333&APPID=f32739e98977f8602979fb6fa944da7f',
+		  dataType: 'json',
+		  success: updatePage,
+		  error: displayError
+		};
+
+		// Initiate the request!
+		$.ajax(weatherOptions);
 	});
 
 })(jQuery, document, window);
